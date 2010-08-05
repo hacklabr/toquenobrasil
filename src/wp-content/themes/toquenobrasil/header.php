b5add7510922693d8eb2723e8355b95081f9d544
andersonorui <andersonorui@b0744693-59ed-4ca4-8b30-5cd01e1ee683>
2010-08-05 12:59:08 +0000

    [add] tema toquenobrasil [add] pasta uploads
    git-svn-id: https://svn.hacklab.com.br/toquenobrasil/trunk@4 b0744693-59ed-4ca4-8b30-5cd01e1ee683

---
+++ src/wp-content/themes/toquenobrasil/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title>
+  <?php
+    global $page, $paged;
+    wp_title( '|', true, 'right' );
+    bloginfo( 'name' );
+
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) )
+      echo " | $site_description";
+
+    if ( $paged >= 2 || $page >= 2 )
+      echo ' | ' . sprintf( __( 'Page %s', 'tnb' ), max( $paged, $page ) );
+  ?>
+</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php
+  if ( is_singular() && get_option( 'thread_comments' ) )
+    wp_enqueue_script( 'comment-reply' );
+  wp_head();
+?>
+</head>
+
+<body <?php body_class(); ?>>
