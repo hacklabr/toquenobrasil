e86c8b887bd90532b6da1a9fea15ef1915771028
andersonorui <andersonorui@b0744693-59ed-4ca4-8b30-5cd01e1ee683>
2010-08-06 19:02:51 +0000

    [add] Commit automatico de fixtures
    git-svn-id: https://svn.hacklab.com.br/toquenobrasil/trunk@7 b0744693-59ed-4ca4-8b30-5cd01e1ee683

--- src/wp-content/themes/toquenobrasil/footer.php
+++ src/wp-content/themes/toquenobrasil/footer.php
@@ -1,2 +1,8 @@
+  <div class="clear"></div>
+  <div class="prepend-top"></div>
+  <div id="footer" class="span-24">
+
+  </div>
+</div>
 </body>
 </html>
--- src/wp-content/themes/toquenobrasil/header.php
+++ src/wp-content/themes/toquenobrasil/header.php
@@ -27,3 +27,65 @@
 </head>
 
 <body <?php body_class(); ?>>
+  <div id="toquenobrasil" class="container prepend-top">
+    <?php theme_image("toquenobrasil2.png"); ?>
+  </div>
+  
+  <div id="content" class="container">
+    <div class="prepend-1 span-14 last">
+      <?php wp_nav_menu(array("theme_location" => "main",
+                              "container" => "div",
+                              "container_id" => "nav-main",
+                              "before" => "<span class='title'>",
+                              "after" => "<span class='shadow'></span></span>",
+                              "link_before" => "<h1>",
+                              "link_after" => "</h1>"
+                             )
+      ) ?>
+      <div class="clear"></div>
+    </div>
+
+    <div class="clear"></div>
+    
+    <div class="span-9 prepend-1">
+      <div class="item green">
+        <div class="title">
+          <div class="shadow"></div>
+          <h1 class="blue">Artistas</h1>
+          <div class="clear"></div>
+        </div>
+        <div class="background">
+          <p>Ac rhoncus aliquam amet quis pulvinar augue pulvinar augue velit! In? Integer pid auctor enim a nunc ridiculus et massa scelerisque ultricies dictumst arcu porta, ut, etiam pellentesque vut integer, pellentesque amet nisi, ut tincidunt scelerisque, purus mattis porta dictumst penatibus, pid, aliquam et? In, proin dictumst ac? Augue porttitor facilisis elementum, dolor mid et egestas magna urna? Aenean tincidunt lorem ac risus porta sit et, augue quis facilisis sit turpis! A? Integer placerat ultricies dignissim, in facilisis enim! Vel.</p>          
+        </div>
+      </div>
+
+      <div class="item blue">
+        <div class="title">
+          <div class="shadow"></div>
+          <h1>Produtores</h1>
+          <div class="clear"></div>
+        </div>
+        <div class="background">
+          <p>Ac rhoncus aliquam amet quis pulvinar augue pulvinar augue velit! In? Integer pid auctor enim a nunc ridiculus et massa scelerisque ultricies dictumst arcu porta, ut, etiam pellentesque vut integer, pellentesque amet nisi, ut tincidunt scelerisque, purus mattis porta dictumst penatibus, pid, aliquam et? In, proin dictumst ac? Augue porttitor facilisis elementum, dolor mid et egestas magna urna? Aenean tincidunt lorem ac risus porta sit et, augue quis facilisis sit turpis! A? Integer placerat ultricies dignissim, in facilisis enim! Vel.</p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="prepend-1 span-13 last">
+      <div class="item yellow">
+        <div class="title">
+          <div class="shadow"></div>
+          <h1>Assista</h1>
+          <div class="clear"></div>
+        </div>
+        <div class="background">
+          <object width="469" height="264"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=10280242&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00ADEF&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=10280242&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=1&amp;color=00ADEF&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="469" height="264"></embed></object>
+          <div class="prepend-top"></div>
+          <p>Ac rhoncus aliquam amet quis pulvinar augue pulvinar augue velit! In? Integer pid auctor enim a nunc ridiculus et massa scelerisque ultricies dictumst arcu porta, ut, etiam pellentesque vut integer, pellentesque amet nisi, ut tincidunt scelerisque, purus mattis porta dictumst penatibus, pid, aliquam et? In, proin dictumst ac? Augue porttitor facilisis elementum, dolor mid et egestas magna urna? Aenean tincidunt lorem ac risus porta sit et, augue quis facilisis sit turpis! A? Integer placerat ultricies dignissim, in facilisis enim! Vel.</p>
+        </div>
+      </div>
+    </div>
+    
+    
+    <div class="clear"></div>
+    <div class="arrow"></div>
