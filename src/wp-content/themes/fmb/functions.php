b384bb147c2c9e78d9195679a36bd0f6103bdce4
catia <catia@b0744693-59ed-4ca4-8b30-5cd01e1ee683>
2010-10-13 20:40:05 +0000

    tnb no rodapé e imagem do login
    git-svn-id: https://svn.hacklab.com.br/toquenobrasil/trunk@194 b0744693-59ed-4ca4-8b30-5cd01e1ee683

---
+++ src/wp-content/themes/fmb/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+
+function tnb_login_head() {
+    ?>
+    <style>
+        #login {width: 400px;}
+        #login h1 {}
+        #login h1 a {background-image: url(<?php echo get_theme_image('fmb.png'); ?> ); height: 133px; width: 400px;}
+    </style>
+    
+    <?php
+}
+
+?>
