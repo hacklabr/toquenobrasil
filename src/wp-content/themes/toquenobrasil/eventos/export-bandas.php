7d734a4d7714305eecd5ce6fbb70bc700bd1aa46
fabio.montefuscolo <fabio.montefuscolo@b0744693-59ed-4ca4-8b30-5cd01e1ee683>
2011-01-11 14:01:37 +0000

    [add] faz exportação de lista de artistas acontecer como html
    git-svn-id: https://svn.hacklab.com.br/toquenobrasil/trunk@375 b0744693-59ed-4ca4-8b30-5cd01e1ee683

--- src/wp-content/themes/toquenobrasil/eventos/export-bandas.php
+++ src/wp-content/themes/toquenobrasil/eventos/export-bandas.php
@@ -1,38 +1,60 @@
 <?php
+    global $post;
+    $filename = "artists_in_{$post->post_name}.xls";
 
     $candidates = get_post_meta(get_the_ID(), $_GET['exportar']);
-    $result = "";
 
-    $fields = array('user_nicename', 'responsavel', 'first_name', 'last_name',
-                    'nickname', 'banda', 'banda_cidade', 'banda_estado',
-                    'origem_cidade', 'origem_estado', //'description',
-                    'estado', 'integrantes', 'site', 'telefone_ddd',
-                    'user_email', 'user_url', 'telefone_ddd', 'telefone',
-                    'aim', 'jabber', 'yim', 'youtube');
+    $fields = array('banda' => __("Banda"),
+                    'profile_link' => __("Link do perfil"),
+                    'responsavel' => __("Responsável"),
+                    'telefone' => __("Telefone"),
+                    'user_email' => __("Email"),
+                    'origem_cidade' => __("Cidade de origem"),
+                    'origem_estado' => __("Estado de origem"),
+                    'origem_pais' => __("Pais de origem"),
+                    'banda_cidade' => __("Cidade de residencia"),
+                    'banda_estado' => __("Estado de residência"),
+                    'banda_pais' => __("País de residência"));
 
-    header('Content-Type: application/csv');
-    header('Content-disposition: attachment; filename=TNB_Users_' . date('Y-m-d') . '.csv');
- 
-    // label das colunas da planilha csv
-    echo implode(';', $fields).';link_perfil';
+    header('Pragma: public'); 
+    header('Cache-Control: no-store, no-cache, must-revalidate');     // HTTP/1.1 
+    header("Pragma: no-cache"); 
+    header("Expires: 0"); 
+    header('Content-Transfer-Encoding: none'); 
+    header('Content-Type: application/vnd.ms-excel; charset=utf-8');                 // This should work for IE & Opera 
+    header("Content-type: application/x-msexcel; charset=utf-8");                    // This should work for the rest 
+    header("Content-Language: pt");
+    header('Content-Disposition: attachment; filename="'.$filename.'"'); 
+?>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Content-Language" content="pt"/>
+    </head>
+    <body>
+        <table width="1000">
+            <thead>
+                <?php foreach($fields as $f): ?>
+                <th><?php echo $f;?></th>
+                <?php endforeach;?>
+            </thead>
+            <tbody>
 
-    foreach($candidates as $candidate) {
-        echo "\n";
-        
-        $data = get_userdata($candidate);
-        
-        if($data) {
-            // transforma o objeto para um array associativo
-            $data = get_object_vars($data);
-
-            foreach($fields as $field) {
-                $value = $data[$field];
-                echo '"' . $value . '"' . ';';
-            }
-
-            // link_perfil
-            echo '"' . get_author_posts_url($candidate) . '"';
-        }
-    }
-    die;
-
+            <?php foreach($candidates as $candidate):
+                $data = get_userdata($candidate);
+                if($data):
+                    // transforma o objeto para um array associativo
+                    $data = get_object_vars($data);
+                    $data['profile_link'] = get_author_posts_url($candidate);
+                    ?>
+                    <tr>
+                    <?php foreach($fields as $key => $field): $value = $data[$key];?>
+                        <td><?php echo $value;?></td>
+                    <?php endforeach;?>
+                    </tr>
+                <?php endif;
+            endforeach;?>
+            </tbody>
+        </table>
+    </body>
+</html>
