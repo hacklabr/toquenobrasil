3537140f9722ff020c76debd9e3e5ef1582e943d
codezone <codezone@b0744693-59ed-4ca4-8b30-5cd01e1ee683>
2011-04-27 16:03:25 +0000

    [fix] facebook like button
    git-svn-id: https://svn.hacklab.com.br/toquenobrasil/trunk@773 b0744693-59ed-4ca4-8b30-5cd01e1ee683

--- src/wp-content/themes/toquenobrasil_v2/page.php
+++ src/wp-content/themes/toquenobrasil_v2/page.php
@@ -5,7 +5,7 @@
         <h1 class="title"><?php the_title(); ?></h1>
         <article id="<?php echo basename(get_permalink()); ?>">
             <div class="content">
-                <iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:30px;" allowTransparency="true"></iframe>
+                <iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;layout=standard&amp;show_faces=false&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:30px;" allowTransparency="true"></iframe>
                 <?php the_content(); ?>
             </div>
         </article>
