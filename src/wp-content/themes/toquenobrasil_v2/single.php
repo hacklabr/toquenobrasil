a0a40d9b3104ff3a97b226bafd4bb0db61f835e9
codezone <codezone@b0744693-59ed-4ca4-8b30-5cd01e1ee683>
2011-04-27 15:10:16 +0000

    [fix] like do facebook
    git-svn-id: https://svn.hacklab.com.br/toquenobrasil/trunk@772 b0744693-59ed-4ca4-8b30-5cd01e1ee683

--- src/wp-content/themes/toquenobrasil_v2/single.php
+++ src/wp-content/themes/toquenobrasil_v2/single.php
@@ -8,7 +8,7 @@
             <p class="post-meta date"><?php the_time("d.m.Y"); ?></p>
             <div class="content">
                 <h1><a href="<?php the_permalink(); ?>" title="<?php echo basename(get_permalink()); ?>"><?php the_title(); ?></a></h1>
-                <iframe src="http://www.facebook.com/plugins/like.php?href<?php the_permalink(); ?>&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:30px;" allowTransparency="true"></iframe>
+                <iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;font&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:30px;" allowTransparency="true"></iframe>
                 <ul class="post-categories clearfix">
                     <?php the_category(); ?>
                 </ul>
