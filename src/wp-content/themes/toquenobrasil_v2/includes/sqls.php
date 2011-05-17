474e39a27c069b794d57619b01ad86e345d7704a
Rafael Freitas <rafael@hacklab.com.br>
2011-05-17 20:03:21 -0300

    [fix] script para corrigir infos de estado e pais dos produtores na tabela wp_tnb_users_stats

--- src/wp-content/themes/toquenobrasil_v2/includes/sqls.php
+++ src/wp-content/themes/toquenobrasil_v2/includes/sqls.php
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS `pagseguro_transacoes` (
 
 
 
-if(!get_option('tnb_sql_29')){ 
-	update_option('tnb_sql_29', 1);
+if(!get_option('tnb_sql_31')){ 
+	update_option('tnb_sql_31', 1);
 	
 	global $wpdb;
 	$sql = "
@@ -98,10 +98,10 @@ WHERE
 	$wpdb->usermeta.meta_key = '{$wpdb->prefix}capabilities' AND
 	($wpdb->usermeta.meta_value LIKE '%artista%' OR
 	 $wpdb->usermeta.meta_value LIKE '%produtor%') AND
-	$wpdb->users.ID NOT INT (SELECT ID FROM $wpdb->users)";
+	$wpdb->users.ID NOT IN (SELECT user_id FROM {$wpdb->prefix}tnb_users_stats)";
 	 
 	 $users = $wpdb->get_results($query);
-	 
+	 _pr($query);
 	 foreach ($users as $user){
 	 	$capability = $user->is_artista ? 'artista' : 'produtor';
 	 	if($capability == 'artista'){
@@ -110,8 +110,8 @@ WHERE
 	 		$cidade = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'banda_cidade' AND user_id = $user->ID");
 	 	}else{
 	 		$pais = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'origem_pais' AND user_id = $user->ID");
-	 		$estado = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'banda_estado' AND user_id = $user->ID");
-	 		$cidade = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'banda_cidade' AND user_id = $user->ID");
+	 		$estado = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'origem_estado' AND user_id = $user->ID");
+	 		$cidade = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = 'origem_cidade' AND user_id = $user->ID");
 	 	} 
 	 	$q = "
 INSERT INTO {$wpdb->prefix}tnb_users_stats(
@@ -135,5 +135,24 @@ INSERT INTO {$wpdb->prefix}tnb_users_stats(
 )";
 	 	$wpdb->query($q);
 	 }
+}
+
+if(!get_option('tnb_sql_32')){ 
+	!update_option('tnb_sql_32', 1);
+	global $wpdb;
+	$produtores = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}tnb_users_stats WHERE capability = 'produtor'");
+	
+	foreach($produtores as $produtor){
+		$origem_pais = '';
+		$origem_estado = '';
+		$origem_cidade = '';
+		
+		$metas = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->usermeta WHERE user_id = '$produtor->user_id' AND (meta_key = 'origem_pais' OR meta_key = 'origem_estado' OR meta_key = 'origem_cidade')");
+		foreach($metas as $meta){
+			$key = $meta->meta_key;
+			$$key = $meta->meta_value;
+		}
+		$wpdb->query("UPDATE {$wpdb->prefix}tnb_users_stats SET pais = '$origem_pais', estado = '$origem_estado', cidade = '$origem_cidade' WHERE user_id = '$produtor->user_id'");
+	}
 }
 ?>
