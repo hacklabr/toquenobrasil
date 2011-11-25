4bd94fc93ff93fe5794c54b68a0ba13556506b80
Rafael Freitas <rafael.chaves.freitas@gmail.com>
2011-11-25 18:42:29 -0200

    [add] exibição por todas as oportunidades

--- src/wp-content/themes/toquenobrasil_v2/includes/admin/pagamento-extratos.php
+++ src/wp-content/themes/toquenobrasil_v2/includes/admin/pagamento-extratos.php
@@ -83,8 +83,6 @@ $cols = array(
     
     .legenda {margin-bottom: 11px}
     .legenda .color-box {width: 20px; height: 20px; float:left; border:1px solid #DFDFDF; }
-    
-    #extrato table tbody 
 </style>
 
 <div class="wrap">
@@ -96,6 +94,7 @@ $cols = array(
     <h2>Extrato de retornos do PagSeguro</h2>
     oportunidade: <select id="extrato-evento-id">
         <option value="">Selecione uma oportunidade</option>
+        <option value="todas" <?php if('todas' == $_GET['evento_id']) echo 'selected="selected"' ?>>Todas</option>
     <?php foreach($eventos as $evento): if($evento->ID == $_GET['evento_id']) $selected_evento = $evento;?>
         <option value = "<?php echo $evento->ID; ?>" <?php if($evento->ID == $_GET['evento_id']) echo 'selected="selected"' ?>><?php echo htmlentities($evento->post_title); ?></option>
     <?php endforeach; ?>
@@ -103,14 +102,26 @@ $cols = array(
     <hr/>
     <div id="extrato">
     <?php 
-    if(isset($selected_evento)): 
+    $aprovadas = array();
+    $canceladas = array();
+    $aguardando = array();
+    
+    if(isset($selected_evento)){
         $aprovadas = $wpdb->get_results("SELECT * FROM pagseguro_transacoes WHERE ProdID = '$selected_evento->ID' AND StatusTransacao = 'Aprovado'");
         $canceladas = $wpdb->get_results("SELECT * FROM pagseguro_transacoes WHERE ProdID = '$selected_evento->ID' AND StatusTransacao = 'Cancelado'");
         $aguardando = $wpdb->get_results("SELECT *, (insert_timestamp < CURRENT_DATE() - INTERVAL 1 WEEK) as verificar FROM pagseguro_transacoes WHERE ProdID = '$selected_evento->ID' AND StatusTransacao = 'Aguardando Pagto' AND TransacaoID NOT IN (SELECT DISTINCT (TransacaoID) FROM pagseguro_transacoes WHERE ProdID = '$selected_evento->ID' AND (StatusTransacao = 'Aprovado' OR StatusTransacao = 'Cancelado')) ");
+        ?><h3>retornos da oportunidade: <?php echo $selected_evento->post_title; ?></h3><?php
         //_pr($canceladas);
+    }elseif($_GET['evento_id'] == 'todas'){
+        $aprovadas = $wpdb->get_results("SELECT pagseguro_transacoes.*, $wpdb->posts.post_title as evento FROM pagseguro_transacoes, $wpdb->posts WHERE pagseguro_transacoes.StatusTransacao = 'Aprovado' AND $wpdb->posts.ID = pagseguro_transacoes.ProdID");
+        $canceladas = $wpdb->get_results("SELECT pagseguro_transacoes.*, $wpdb->posts.post_title as evento FROM pagseguro_transacoes, $wpdb->posts WHERE pagseguro_transacoes.StatusTransacao = 'Cancelado' AND $wpdb->posts.ID = pagseguro_transacoes.ProdID");
+        $aguardando = $wpdb->get_results("SELECT pagseguro_transacoes.*, (pagseguro_transacoes.insert_timestamp < CURRENT_DATE() - INTERVAL 1 WEEK) as verificar, $wpdb->posts.post_title as evento FROM pagseguro_transacoes, $wpdb->posts WHERE pagseguro_transacoes.StatusTransacao = 'Aguardando Pagto' AND TransacaoID NOT IN (SELECT DISTINCT (TransacaoID) FROM pagseguro_transacoes WHERE (StatusTransacao = 'Aprovado' OR StatusTransacao = 'Cancelado')) AND $wpdb->posts.ID = pagseguro_transacoes.ProdID");
+        $cols = array('evento' => "Oportunidade") + $cols;
         
+        ?><h3>retornos de todas as oportunidades pagas</h3><?php
+    }
         ?>
-        <h3>retornos da oportunidade: <?php echo $selected_evento->post_title; ?></h3>
+        
         <div class="legenda"><div class="verificar color-box">&nbsp;</div> &nbsp; <small>Transações que estão aguardando há mais de uma semana o retorno de confirmação. Verificar no PagSeguro pela TransaçãoID e atualizar status se necessário.</small></div>
         <div class="legenda"><div class="aguardando color-box">&nbsp;</div> &nbsp; <small>Transações que estão aguardando há menos de uma semana o retorno de confirmação. </small></div>
         <div class="legenda"><div class="aprovadas color-box">&nbsp;</div> &nbsp; <small>Transações aprovadas.</small></div>
@@ -127,7 +138,7 @@ $cols = array(
             
             <?php if($aguardando): ?>
                 <tbody class='aguardando'>
-                    <tr><th colspan="<?php echo count($cols); ?>">Aguardando</th></tr>
+                    <tr><th colspan="<?php echo count($cols); ?>" class="secao-th">Aguardando</th></tr>
                 <?php foreach($aguardando as $trans): ?>
                     <tr <?php if($trans->verificar) echo 'class="verificar"'?>>
                     <?php foreach($cols as $col => $label): ?>
@@ -149,7 +160,7 @@ $cols = array(
                 
             <?php if($aprovadas): ?>
                 <tbody class='aprovadas'>
-                    <tr><th colspan="<?php echo count($cols); ?>">Aprovadas</th></tr>
+                    <tr><th colspan="<?php echo count($cols); ?>" class="secao-th">Aprovadas</th></tr>
                 <?php foreach($aprovadas as $trans): ?>
                     <tr>
                     <?php foreach($cols as $col => $label): ?>
@@ -162,7 +173,7 @@ $cols = array(
                 
             <?php if($canceladas): ?>
                 <tbody class='canceladas'>
-                    <tr><th colspan="<?php echo count($cols); ?>">Canceladas</th></tr>
+                    <tr><th colspan="<?php echo count($cols); ?>" class="secao-th">Canceladas</th></tr>
                 <?php foreach($canceladas as $trans): ?>
                     <tr>
                     <?php foreach($cols as $col => $label): ?>
@@ -173,7 +184,7 @@ $cols = array(
                 </tbody>
             <?php endif; ?>
         </table>
-    <?php endif; ?>
+    
     </div>
 </div>
 
