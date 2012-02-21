a19e03ac22f753d938f4efb658f4adb9f47a76aa
Leo Germani <leogermani@hacklab.com.br>
2012-02-21 12:07:30 -0200

    [fix] nome do arquivo mp3 a hora do download

--- src/wp-content/themes/toquenobrasil_v2/media/download.php
+++ src/wp-content/themes/toquenobrasil_v2/media/download.php
@@ -14,9 +14,11 @@ do_action('download_music', $_GET['id']);
 
 $mime = $post->post_mime_type;
 
+$filename = preg_replace('|^http://[^/]+/wp-content/uploads/\d{4}/\d\d/(.+)(-[a-zA-Z0-9]{13}).mp3$|', "$1.mp3", $file_url);
+
 header("Cache-Control: public");
 header("Content-Description: File Transfer");
-header("Content-Disposition: attachment; filename=$file_url");
+header("Content-Disposition: attachment; filename=$filename");
 header("Content-Type: $mime");
 header("Content-Transfer-Encoding: binary");
 
